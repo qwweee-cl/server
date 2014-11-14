ae5f892013d591f3332442a6af529c4d14f8dbea
hadoop <hadoop@cat.clt.gocyberlink.com>
2014-11-14 02:34:14 +0000

    [what] add sharding key in createIndex.js

--- api/createIndex.js
+++ api/createIndex.js
@@ -19,6 +19,7 @@ function executeCmd(cmd) {
 eventEmitter.on('executeCmd', executeCmd);
 
 var mongoUrl=config.mongodb.hostbatch+":"+config.mongodb.port+"/"+config.mongodb.db_batch;
+var shardingUrl=config.mongodb.hostbatch+":30000/admin";
 
 dbonoff.open(common.db_batch);
 common.db_batch.collections(function(err,collection) {
@@ -30,22 +31,28 @@ common.db_batch.collections(function(err,collection) {
     }
     var eventCmd = '/usr/bin/mongo '+mongoUrl+' --eval "\n';
     var sessionCmd = '/usr/bin/mongo '+mongoUrl+' --eval "\n';
+    var shardingCmd = '/usr/bin/mongo '+shardingUrl+' --eval "\n';
     for (var i=0; i<collection.length; i++) {
         var collectionName = collection[i].collectionName;
         if (collectionName.indexOf(common.rawCollection['event'])>=0) {
             //print("Entering event :"+collectionName);
             eventCmd += "db."+collectionName+".ensureIndex({app_user_id:1});\n";
+            shardingCmd += "sh.shardCollection('clad_raw00."+collectionName+"', {app_user_id:1});\n";
         } else if (collectionName.indexOf(common.rawCollection['session'])>=0) {
             //print("Entering sessions :"+collectionName);
             sessionCmd += "db."+collectionName+".ensureIndex({app_user_id:1,timestamp:1});\n";
+            shardingCmd += "sh.shardCollection('clad_raw00."+collectionName+"', {app_user_id:1});\n";
         }
     }
     eventCmd += '"';
     sessionCmd += '"';
+    shardingCmd += '"';
     //print(eventCmd);
     eventEmitter.emit('executeCmd', eventCmd);
     //print(sessionCmd);
     eventEmitter.emit('executeCmd', sessionCmd);
+    //print(shardingCmd);
+    //eventEmitter.emit('executeCmd', shardingCmd);
 
     dbonoff.close(common.db_batch);
 });
