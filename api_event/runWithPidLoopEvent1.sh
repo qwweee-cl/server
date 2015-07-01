c35f4e7f085f89c4d8e1e3d9c878cc7ecb12beab
hadoop <hadoop@cat.pft.com>
2015-07-01 08:43:13 +0000

    [What] Modify runWithPidLoopEvent* for process event on api_event folder

--- api_event/runWithPidLoopEvent1.sh
+++ api_event/runWithPidLoopEvent1.sh
@@ -10,5 +10,5 @@ if [ -e ${LOCKFILE} ] ; then
 	exit 1
 fi
 
-nohup /usr/local/countly/api/loopEventBatch.sh >> /usr/local/countly/log/loopEventMain1.log 2>&1 & echo $! > /tmp/loopEventBatch1.pid
+nohup /usr/local/countly/api_event/loopEventBatch.sh >> /usr/local/countly/log/loopEventMain1.log 2>&1 & echo $! > /tmp/loopEventBatch1.pid
 
--- api_event/runWithPidLoopEvent2.sh
+++ api_event/runWithPidLoopEvent2.sh
@@ -10,5 +10,5 @@ if [ -e ${LOCKFILE} ] ; then
 	exit 1
 fi
 
-nohup /usr/local/countly/api/loopEventBatch2.sh >> /usr/local/countly/log/loopEventMain2.log 2>&1 & echo $! > /tmp/loopEventBatch2.pid
+nohup /usr/local/countly/api_event/loopEventBatch2.sh >> /usr/local/countly/log/loopEventMain2.log 2>&1 & echo $! > /tmp/loopEventBatch2.pid
 
