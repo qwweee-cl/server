348b82a55bf12b604567cf28e32f5ff20535600c
hadoop <hadoop@cat.pft.com>
2015-09-22 07:02:11 +0000

    [What] 1. copy to s3 and emr with date(0101) paramater        2. create a script for run copy to emr and execute compute predict [Why ] 1. execute compute need a date(20150101) paramater        2. split script. Main script is predictionBatch.sh [How ] 1. change prediction copy script with paramater        2. Main script will copy file to emr and execute compute

--- api/predictCopy2emr.sh
+++ api/predictCopy2emr.sh
@@ -22,40 +22,45 @@ working_dir="/usr/local/countly/api"
 mail_target=${AWSM}
 s3PredictionPath="/s3mnt/shard_backup/Old/"
 
+if [ -z "$1" ]; then
+  echo "Please execute with date(0101) paramater"
+  exit 0
+fi
+
 cd ${working_dir}
 
 for i in *.txt ; do
-  regex="[0-9]{4}_543f37d0a62268c51e16d053"
+  regex="$1_543f37d0a62268c51e16d053"
   if [[ "$i" =~ $regex ]]
   then
     echo -e "scp ${i} emr:/data/owl/predict/data/2015${i}"
     scp ${i} emr:/data/owl/predict/data/2015${i}
   fi
-  regex="[0-9]{4}_543f8693a9e5b7ed76000012"
+  regex="$1_543f8693a9e5b7ed76000012"
   if [[ "$i" =~ $regex ]]
   then
     echo -e "scp ${i} emr:/data/owl/predict/data/2015${i}"
     scp ${i} emr:/data/owl/predict/data/2015${i}
   fi
-  regex="[0-9]{4}_543f37eaa62268c51e16d0c3"
+  regex="$1_543f37eaa62268c51e16d0c3"
   if [[ "$i" =~ $regex ]]
   then
     echo -e "scp ${i} emr:/data/owl/predict/data/2015${i}"
     scp ${i} emr:/data/owl/predict/data/2015${i}
   fi
-  regex="[0-9]{4}_543f866fa9e5b7ed76000011"
+  regex="$1_543f866fa9e5b7ed76000011"
   if [[ "$i" =~ $regex ]]
   then
     echo -e "scp ${i} emr:/data/owl/predict/data/2015${i}"
     scp ${i} emr:/data/owl/predict/data/2015${i}
   fi
-  regex="[0-9]{4}_5551e55cacdd571e2e000443"
+  regex="$1_5551e55cacdd571e2e000443"
   if [[ "$i" =~ $regex ]]
   then
     echo -e "scp ${i} emr:/data/owl/predict/data/2015${i}"
     scp ${i} emr:/data/owl/predict/data/2015${i}
   fi
-  regex="[0-9]{4}_5552bf53acdd571e2e00044e"
+  regex="$1_5552bf53acdd571e2e00044e"
   if [[ "$i" =~ $regex ]]
   then
     echo -e "scp ${i} emr:/data/owl/predict/data/2015${i}"
---
+++ api/predictionBatch.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+. /usr/local/countly/api/maillist.sh
+trap 'error_exp'  ERR SIGINT SIGTERM
+
+function error_exp
+{
+  echo -e "error"
+  echo -e "Execute Prediction Error ${mainLogFile} : "\
+  $(tail -20 ${mainLogFile})
+  | mail -s "[Old] Error Execute Prediction" ${AWSM}
+  exit 0
+}
+
+function sendSummaryMail() {
+  echo -e "summary"
+  echo -e "Execute Prediction logs ${mainLogFile} : "\
+  $(tail -20 ${mainLogFile}) \
+  | mail -s "[Old] Execute Prediction on EMR Summary" ${AWSM}
+}
+
+mainLogFile="/usr/local/countly/log/shardPredictionExe.log"
+working_dir="/usr/local/countly/api"
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Please execute with dbname and index(1 or 2) paramater"
+  exit 0
+fi
+
+cd ${working_dir}
+string="$1"
+regular="[0-9]{4}"
+if [[ $string =~ $regular ]]; then 
+  execDate="2015${BASH_REMATCH[0]}"
+  echo ${execDate}
+  echo ${execDate} >> ${mainLogFile}
+fi
+
+## execute copy prediction file to s3
+#cmd="./predictCopy2S3.sh"
+#echo -e "${cmd}" >> ${mainLogFile}
+#${cmd}
+## execute copy prediction file to emr
+cmd="./predictCopy2emr.sh"
+echo -e "${cmd}" >> ${mainLogFile}
+${cmd}
+
+if [ "$2" == "2" ]; then
+  echo -e "Execute Prediction Scirpt"
+  echo -e "Execute Prediction Scirpt" >> ${mainLogFile}
+  cmd="ssh ubuntu@emr /home/ubuntu/predict/daily_predict_APP.sh ${execDate} >> /data/owl/predict/log/sshCallPredict.log"
+  echo -e "${cmd}" >> ${mainLogFile}
+  ${cmd}
+fi
+
