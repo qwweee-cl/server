dd6427eb9a320bcb920718fb7f73f1596820a54b
hadoop <hadoop@cat.pft.com>
2015-09-22 03:38:25 +0000

    [What] add for prediction file copy to s3 and copy to emr [Why ] for prediction [How ] cp prediction files to s3 and scp prediction files to emr

---
+++ api/predictCopy2S3.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+. /usr/local/countly/api/maillist.sh
+trap 'error_exp'  ERR SIGINT SIGTERM
+
+function error_exp
+{
+  echo -e "[Old] Prediction Copy to S3 fail"\
+  $(tail -20 ${mainLogFile}) \
+  | mail -s "[Old] Prediction Copy to S3 Error Trap" ${mail_target}
+  echo -e "Copy Prediction files error!"
+  exit 1
+}
+
+function sendSummaryMail() {
+  echo -e "Prediction logs ${mainLogFile} : "\
+  $(tail -20 ${mainLogFile}) \
+  | mail -s "[Old] Prediction Copy to S3 Summary" ${AWSM}
+}
+
+mainLogFile="/usr/local/countly/log/shardPrediction.log"
+working_dir="/usr/local/countly/api"
+mail_target=${AWSM}
+s3PredictionPath="/s3mnt/shard_backup/Old/"
+
+cd ${working_dir}
+
+for i in *.txt ; do
+  regex="[0-9]{4}_543f37d0a62268c51e16d053"
+  if [[ "$i" =~ $regex ]]
+  then
+    echo -e "cp ${i} ${s3PredictionPath}2015${i}"
+    cp ${i} ${s3PredictionPath}2015${i}
+  fi
+  regex="[0-9]{4}_543f8693a9e5b7ed76000012"
+  if [[ "$i" =~ $regex ]]
+  then
+    echo -e "cp ${i} ${s3PredictionPath}2015${i}"
+    cp ${i} ${s3PredictionPath}2015${i}
+  fi
+  regex="[0-9]{4}_543f37eaa62268c51e16d0c3"
+  if [[ "$i" =~ $regex ]]
+  then
+    echo -e "cp ${i} ${s3PredictionPath}2015${i}"
+    cp ${i} ${s3PredictionPath}2015${i}
+  fi
+  regex="[0-9]{4}_543f866fa9e5b7ed76000011"
+  if [[ "$i" =~ $regex ]]
+  then
+    echo -e "cp ${i} ${s3PredictionPath}2015${i}"
+    cp ${i} ${s3PredictionPath}2015${i}
+  fi
+  regex="[0-9]{4}_5551e55cacdd571e2e000443"
+  if [[ "$i" =~ $regex ]]
+  then
+    echo -e "cp ${i} ${s3PredictionPath}2015${i}"
+    cp ${i} ${s3PredictionPath}2015${i}
+  fi
+  regex="[0-9]{4}_5552bf53acdd571e2e00044e"
+  if [[ "$i" =~ $regex ]]
+  then
+    echo -e "cp ${i} ${s3PredictionPath}2015${i}"
+    cp ${i} ${s3PredictionPath}2015${i}
+  fi
+done
+
+
