f131ab40f6b719ad12e4e8d280c3ae8231b48da7
hadoop <hadoop@cat.pft.com>
2015-09-24 03:51:52 +0000

    [What] enable save to shard [Why ] finished to change shard instance type [How ] mark if (0) when write to shard

--- api/api.js
+++ api/api.js
@@ -133,7 +133,7 @@ function insertRawColl(coll, eventp, params) {
             }
         });
     }
-    if (0)
+    //if (0)
     {
         common.getShardRawDB(eventp.app_key).collection(coll).insert(eventp, function(err, res) {
             if (err) {
