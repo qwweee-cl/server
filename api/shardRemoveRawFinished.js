0a181b6284a91359bd157699e993263f9391715b
hadoop <hadoop@cat.pft.com>
2015-11-19 17:10:23 +0000

    [What] when finished backup raw will insert to backup_finished_test(1 or 2) collection for test [Why ] for session test [How ] add update backup_finished_test(1 or 2) collection on shardRemoveRawFinished.js

--- api/shardRemoveRawFinished.js
+++ api/shardRemoveRawFinished.js
@@ -10,6 +10,7 @@ if (process.argv.length != 4) {
 var indbname = process.argv[2];
 var collectionName = 'raw_finished'+process.argv[3];
 var backupColName = "backup_finished"+process.argv[3];
+var backupColNameTest = "backup_finished_test"+process.argv[3];
 query = {dbname: indbname};
 //print(query);
 
@@ -32,6 +33,16 @@ common.db_maintain.collection(backupColName).update({dbname: indbname},
 {$set: insertData}, {'upsert': true}, function(err, res) {
     if (err) {
         print('backup_finished error');
+        print(err);
+    }
+    dbonoff.close(common.db_maintain);
+});
+
+dbonoff.open(common.db_maintain);
+common.db_maintain.collection(backupColNameTest).update({dbname: indbname},
+{$set: insertData}, {'upsert': true}, function(err, res) {
+    if (err) {
+        print('backup_finished_test error');
         print(err);
     }
     dbonoff.close(common.db_maintain);
