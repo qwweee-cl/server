ae2e91c4f68fbfc0c2f87bfc08262799e0db0cae
hadoop <hadoop@cat.pft.com>
2015-11-30 11:12:05 +0000

    [What] drop shrad raw database using aws-cli [Why ] not use s3fs [How ] use aws-cli to get file exist and file size

--- api/dropMongoDBRawShard.sh
+++ api/dropMongoDBRawShard.sh
@@ -23,6 +23,8 @@ cd ${path}
 
 mongo="localhost:27017"
 s3Path="/s3mnt/shard_backup/hourly_data/"
+cmds3DashboardPath="/s3mnt/shard_backup/dashboard_data/"
+cmds3Path="s3://clcom2-countly/shard_backup/hourly_data/"
 
 fullCurDate=$(date -d "-3 days" +%Y%m%d)
 
@@ -51,15 +53,23 @@ echo -e ${four}
 
 s3One1=${s3Path}${fullCurDate}"_raw_00_1.tgz"
 s3One2=${s3Path}${fullCurDate}"_raw_00_2.tgz"
+s3One1=${cmds3Path}${fullCurDate}"_raw_00_1.tgz"
+s3One2=${cmds3Path}${fullCurDate}"_raw_00_2.tgz"
 
 s3Two1=${s3Path}${fullCurDate}"_raw_01_1.tgz"
 s3Two2=${s3Path}${fullCurDate}"_raw_01_2.tgz"
+s3Two1=${cmds3Path}${fullCurDate}"_raw_01_1.tgz"
+s3Two2=${cmds3Path}${fullCurDate}"_raw_01_2.tgz"
 
 s3Three1=${s3Path}${fullCurDate}"_raw_02_1.tgz"
 s3Three2=${s3Path}${fullCurDate}"_raw_02_2.tgz"
+s3Three1=${cmds3Path}${fullCurDate}"_raw_02_1.tgz"
+s3Three2=${cmds3Path}${fullCurDate}"_raw_02_2.tgz"
 
 s3Four1=${s3Path}${fullCurDate}"_raw_03_1.tgz"
 s3Four2=${s3Path}${fullCurDate}"_raw_03_2.tgz"
+s3Four1=${cmds3Path}${fullCurDate}"_raw_03_1.tgz"
+s3Four2=${cmds3Path}${fullCurDate}"_raw_03_2.tgz"
 
 echo -e ${s3One1}
 echo -e ${s3One2}
@@ -70,43 +80,97 @@ echo -e ${s3Three2}
 echo -e ${s3Four1}
 echo -e ${s3Four2}
 
+existFile1_1=`aws s3 ls ${s3One1} | wc -l`
+existFile1_2=`aws s3 ls ${s3One2} | wc -l`
+existFile2_1=`aws s3 ls ${s3Two1} | wc -l`
+existFile2_2=`aws s3 ls ${s3Two2} | wc -l`
+existFile3_1=`aws s3 ls ${s3Three1} | wc -l`
+existFile3_2=`aws s3 ls ${s3Three2} | wc -l`
+existFile4_1=`aws s3 ls ${s3Four1} | wc -l`
+existFile4_2=`aws s3 ls ${s3Four2} | wc -l`
+
 fileExist=true
 
-if [ ! -f ${s3One1} ] || [ ! -f ${s3One2} ]; then
+if [ ${existFile1_1} == "0" ] || [ ${existFile1_2} == "0" ]; then
 	echo "${s3One1} or ${s3One2} file not exist" >> ${one_time_log}
 	fileExist=false
 fi
-if [ ! -f ${s3Two1} ] || [ ! -f ${s3Two2} ]; then
+if [ ${existFile2_1} == "0" ] || [ ${existFile2_2} == "0" ]; then
 	echo "${s3Two1} or ${s3Two2} file not exist" >> ${one_time_log}
 	fileExist=false
 fi
-if [ ! -f ${s3Three1} ] || [ ! -f ${s3Three2} ]; then
+if [ ${existFile3_1} == "0" ] || [ ${existFile3_2} == "0" ]; then
 	echo "${s3Three1} or ${s3Three2} file not exist" >> ${one_time_log}
 	fileExist=false
 fi
-if [ ! -f ${s3Four1} ] || [ ! -f ${s3Four2} ]; then
+if [ ${existFile4_1} == "0" ] || [ ${existFile4_2} == "0" ]; then
 	echo "${s3Four1} or ${s3Four2} file not exist" >> ${one_time_log}
 	fileExist=false
 fi
+
+## use s3fs to get file information
+#if [ ! -f ${s3One1} ] || [ ! -f ${s3One2} ]; then
+#	echo "${s3One1} or ${s3One2} file not exist" >> ${one_time_log}
+#	fileExist=false
+#fi
+#if [ ! -f ${s3Two1} ] || [ ! -f ${s3Two2} ]; then
+#	echo "${s3Two1} or ${s3Two2} file not exist" >> ${one_time_log}
+#	fileExist=false
+#fi
+#if [ ! -f ${s3Three1} ] || [ ! -f ${s3Three2} ]; then
+#	echo "${s3Three1} or ${s3Three2} file not exist" >> ${one_time_log}
+#	fileExist=false
+#fi
+#if [ ! -f ${s3Four1} ] || [ ! -f ${s3Four2} ]; then
+#	echo "${s3Four1} or ${s3Four2} file not exist" >> ${one_time_log}
+#	fileExist=false
+#fi
 status=1
 string="File Is Zero";
 
-if [ ! -s ${s3One1} ] || [ ! -s ${s3One2} ]; then
-	echo "${s3One1} or ${s3One2} file size is 0" >> ${one_time_log}
+duFile1_1=`aws s3 ls ${s3One1} | awk '{ print $3 }'`
+duFile1_2=`aws s3 ls ${s3One2} | awk '{ print $3 }'`
+duFile2_1=`aws s3 ls ${s3Two1} | awk '{ print $3 }'`
+duFile2_2=`aws s3 ls ${s3Two2} | awk '{ print $3 }'`
+duFile3_1=`aws s3 ls ${s3Three1} | awk '{ print $3 }'`
+duFile3_2=`aws s3 ls ${s3Three2} | awk '{ print $3 }'`
+duFile4_1=`aws s3 ls ${s3Four1} | awk '{ print $3 }'`
+duFile4_2=`aws s3 ls ${s3Four2} | awk '{ print $3 }'`
+
+if [ -z ${duFile1_1} ] || [ -z ${duFile1_2} ]; then
+	echo "${s3One1} or ${s3One1} file size is 0" >> ${one_time_log}
 	fileExist=false
 fi
-if [ ! -s ${s3Two1} ] || [ ! -s ${s3Two2} ]; then
+if [ -z ${duFile2_1} ] || [ -z ${duFile2_2} ]; then
 	echo "${s3Two1} or ${s3Two2} file size is 0" >> ${one_time_log}
 	fileExist=false
 fi
-if [ ! -s ${s3Three1} ] || [ ! -s ${s3Three2} ]; then
+if [ -z ${duFile3_1} ] || [ -z ${duFile3_2} ]; then
 	echo "${s3Three1} or ${s3Three2} file size is 0" >> ${one_time_log}
 	fileExist=false
 fi
-if [ ! -s ${s3Four1} ] || [ ! -s ${s3Four2} ]; then
+if [ -z ${duFile4_1} ] || [ -z ${duFile4_2} ]; then
 	echo "${s3Four1} or ${s3Four2} file size is 0" >> ${one_time_log}
 	fileExist=false
 fi
+
+## use s3fs to get file information
+#if [ ! -s ${s3One1} ] || [ ! -s ${s3One2} ]; then
+#	echo "${s3One1} or ${s3One1} file size is 0" >> ${one_time_log}
+#	fileExist=false
+#fi
+#if [ ! -s ${s3Two1} ] || [ ! -s ${s3Two2} ]; then
+#	echo "${s3Two1} or ${s3Two2} file size is 0" >> ${one_time_log}
+#	fileExist=false
+#fi
+#if [ ! -s ${s3Three1} ] || [ ! -s ${s3Three2} ]; then
+#	echo "${s3Three1} or ${s3Three2} file size is 0" >> ${one_time_log}
+#	fileExist=false
+#fi
+#if [ ! -s ${s3Four1} ] || [ ! -s ${s3Four2} ]; then
+#	echo "${s3Four1} or ${s3Four2} file size is 0" >> ${one_time_log}
+#	fileExist=false
+#fi
 
 echo -e ${fileExist}
 
@@ -116,14 +180,23 @@ if [ ${fileExist} = false ]; then
 	exit 0
 fi
 
-sizeOne1=$(du -b "${s3One1}" | cut -f 1)
-sizeOne2=$(du -b "${s3One2}" | cut -f 1)
-sizeTwo1=$(du -b "${s3Two1}" | cut -f 1)
-sizeTwo2=$(du -b "${s3Two2}" | cut -f 1)
-sizeThree1=$(du -b "${s3Three1}" | cut -f 1)
-sizeThree2=$(du -b "${s3Three2}" | cut -f 1)
-sizeFour1=$(du -b "${s3Four1}" | cut -f 1)
-sizeFour2=$(du -b "${s3Four2}" | cut -f 1)
+sizeOne1=${duFile1_1}
+sizeOne2=${duFile1_2}
+sizeTwo1=${duFile2_1}
+sizeTwo2=${duFile2_2}
+sizeThree1=${duFile3_1}
+sizeThree2=${duFile3_2}
+sizeFour1=${duFile4_1}
+sizeFour2=${duFile4_2}
+
+#sizeOne1=$(du -b "${s3One1}" | cut -f 1)
+#sizeOne2=$(du -b "${s3One2}" | cut -f 1)
+#sizeTwo1=$(du -b "${s3Two1}" | cut -f 1)
+#sizeTwo2=$(du -b "${s3Two2}" | cut -f 1)
+#sizeThree1=$(du -b "${s3Three1}" | cut -f 1)
+#sizeThree2=$(du -b "${s3Three2}" | cut -f 1)
+#sizeFour1=$(du -b "${s3Four1}" | cut -f 1)
+#sizeFour2=$(du -b "${s3Four2}" | cut -f 1)
 
 echo -e "${s3One1} : ${sizeOne1}" >> ${one_time_log}
 echo -e "${s3One2} : ${sizeOne2}" >> ${one_time_log}
