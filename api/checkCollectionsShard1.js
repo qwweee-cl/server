ac716cdf032bc6443205a724250ec472e8663d36
hadoop <hadoop@cat.pft.com>
2015-12-18 11:19:53 +0000

    [What] Fix checkCollection nodejs script bug

--- api/checkCollectionsShard1.js
+++ api/checkCollectionsShard1.js
@@ -4,16 +4,6 @@ var mongo = require('mongoskin'),
 var dbHost = "shard1-0";
 var dbOptions = { safe:false, maxPoolSize: 1000 };
 var dbName = "";
-var app = ["0368eb926b115ecaf41eff9a0536a332ef191417",
-           "02ce3171f470b3d638feeaec0b3f06bd27f86a26",
-           "9219f32e8de29b826faf44eb9b619788e29041bb",
-           "75edfca17dfbe875e63a66633ed6b00e30adcb92",
-           "c277de0546df31757ff26a723907bc150add4254",
-           "e315c111663af26a53e5fe4c82cc1baeecf50599",
-           "895ef49612e79d93c462c6d34abd8949b4c849af",
-           "ecc26ef108c821f3aadc5e283c512ee68be7d43e",
-           "488fea5101de4a8226718db0611c2ff2daeca06a",
-           "7cd568771523a0621abff9ae3f95daf3a8694392"];
 
 var appKey = ["raw_session_0368eb926b115ecaf41eff9a0536a332ef191417",
               "raw_session_02ce3171f470b3d638feeaec0b3f06bd27f86a26",
@@ -52,20 +42,19 @@ db.collections(function(err,collection) {
 		process.exit(0);
 	}
 	var isNotExist = false;
-	for (var i=0; i<collection.length; i++) {
-		for (var j=0;j<app.length;j++) {
-			if (collection[i].collectionName.indexOf(app[j])>=0) {
-				for (var k=0;k<appKey.length;k++) {
-					if (collection[i].collectionName.indexOf(appKey[k])>=0) {
-//print("session: "+collection[i].collectionName);
-					} else {
-						print(collection[i].collectionName);
-						isNotExist = true;
-					}
-				}
-			}
+	var srcArray=[];
+
+	for (var i=0;i<collection.length;i++) {
+		srcArray.push(collection[i].collectionName);
+	}
+
+	for (var i=0;i<appKey.length;i++) {
+		if (srcArray.indexOf(appKey[i])==-1) {
+			print(appKey[i]);
+			isNotExist = true;
 		}
 	}
+
 	if (isNotExist) {
 		process.exit(1);
 	}
