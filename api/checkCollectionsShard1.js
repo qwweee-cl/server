9103c5a3f5d3da7a7d7f4b1c0d0839a95c0b836e
hadoop <hadoop@cat.pft.com>
2015-12-18 10:09:41 +0000

    [What] Fix checkCollection nodejs script bug

--- api/checkCollectionsShard1.js
+++ api/checkCollectionsShard1.js
@@ -4,6 +4,17 @@ var mongo = require('mongoskin'),
 var dbHost = "shard1-0";
 var dbOptions = { safe:false, maxPoolSize: 1000 };
 var dbName = "";
+var app = ["0368eb926b115ecaf41eff9a0536a332ef191417",
+           "02ce3171f470b3d638feeaec0b3f06bd27f86a26",
+           "9219f32e8de29b826faf44eb9b619788e29041bb",
+           "75edfca17dfbe875e63a66633ed6b00e30adcb92",
+           "c277de0546df31757ff26a723907bc150add4254",
+           "e315c111663af26a53e5fe4c82cc1baeecf50599",
+           "895ef49612e79d93c462c6d34abd8949b4c849af",
+           "ecc26ef108c821f3aadc5e283c512ee68be7d43e",
+           "488fea5101de4a8226718db0611c2ff2daeca06a",
+           "7cd568771523a0621abff9ae3f95daf3a8694392"];
+
 var appKey = ["raw_session_0368eb926b115ecaf41eff9a0536a332ef191417",
               "raw_session_02ce3171f470b3d638feeaec0b3f06bd27f86a26",
               "raw_session_9219f32e8de29b826faf44eb9b619788e29041bb",
