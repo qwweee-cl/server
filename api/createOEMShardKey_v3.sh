8b8dfe588f1f78b65e7d3b33dbef51574527673e
hadoop <hadoop@cat.pft.com>
2016-05-05 07:56:37 +0000

    [What] Fix createOEMShardKey_v3.sh to create session & event index [Why ] event not create index [How ] add ensureEventIndex.js, ensureSessionIndex.js for create Event & Session index

--- api/createOEMShardKey_v3.sh
+++ api/createOEMShardKey_v3.sh
@@ -66,26 +66,47 @@ for (( j = 0 ; j < ${#oems[@]} ; j++ )) do
   dbHost="localhost:30000"
 
   batchdb="${dbName0}"
-  echo -e "node ensureIndex.js ${dbHost} ${batchdb} raw_session_e315c111663af26a53e5fe4c82cc1baeecf50599" >> ${one_time_log}
-  echo -e "node ensureIndex.js ${dbHost} ${batchdb} raw_session_c277de0546df31757ff26a723907bc150add4254" >> ${one_time_log}
-  echo -e "node ensureIndex.js ${dbHost} ${batchdb} raw_session_75edfca17dfbe875e63a66633ed6b00e30adcb92" >> ${one_time_log}
-  echo -e "node ensureIndex.js ${dbHost} ${batchdb} raw_session_9219f32e8de29b826faf44eb9b619788e29041bb" >> ${one_time_log}
-  echo -e "node ensureIndex.js ${dbHost} ${batchdb} raw_session_0368eb926b115ecaf41eff9a0536a332ef191417" >> ${one_time_log}
-  echo -e "node ensureIndex.js ${dbHost} ${batchdb} raw_session_02ce3171f470b3d638feeaec0b3f06bd27f86a26" >> ${one_time_log}
-  echo -e "node ensureIndex.js ${dbHost} ${batchdb} raw_session_895ef49612e79d93c462c6d34abd8949b4c849af" >> ${one_time_log}
-  echo -e "node ensureIndex.js ${dbHost} ${batchdb} raw_session_ecc26ef108c821f3aadc5e283c512ee68be7d43e" >> ${one_time_log}
-  echo -e "node ensureIndex.js ${dbHost} ${batchdb} raw_session_488fea5101de4a8226718db0611c2ff2daeca06a" >> ${one_time_log}
-  echo -e "node ensureIndex.js ${dbHost} ${batchdb} raw_session_7cd568771523a0621abff9ae3f95daf3a8694392" >> ${one_time_log}
-  node ensureIndex.js ${dbHost} ${batchdb} raw_session_e315c111663af26a53e5fe4c82cc1baeecf50599
-  node ensureIndex.js ${dbHost} ${batchdb} raw_session_c277de0546df31757ff26a723907bc150add4254
-  node ensureIndex.js ${dbHost} ${batchdb} raw_session_75edfca17dfbe875e63a66633ed6b00e30adcb92
-  node ensureIndex.js ${dbHost} ${batchdb} raw_session_9219f32e8de29b826faf44eb9b619788e29041bb
-  node ensureIndex.js ${dbHost} ${batchdb} raw_session_0368eb926b115ecaf41eff9a0536a332ef191417
-  node ensureIndex.js ${dbHost} ${batchdb} raw_session_02ce3171f470b3d638feeaec0b3f06bd27f86a26
-  node ensureIndex.js ${dbHost} ${batchdb} raw_session_895ef49612e79d93c462c6d34abd8949b4c849af
-  node ensureIndex.js ${dbHost} ${batchdb} raw_session_ecc26ef108c821f3aadc5e283c512ee68be7d43e
-  node ensureIndex.js ${dbHost} ${batchdb} raw_session_488fea5101de4a8226718db0611c2ff2daeca06a
-  node ensureIndex.js ${dbHost} ${batchdb} raw_session_7cd568771523a0621abff9ae3f95daf3a8694392
+  echo -e "node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_e315c111663af26a53e5fe4c82cc1baeecf50599" >> ${one_time_log}
+  echo -e "node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_c277de0546df31757ff26a723907bc150add4254" >> ${one_time_log}
+  echo -e "node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_75edfca17dfbe875e63a66633ed6b00e30adcb92" >> ${one_time_log}
+  echo -e "node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_9219f32e8de29b826faf44eb9b619788e29041bb" >> ${one_time_log}
+  echo -e "node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_0368eb926b115ecaf41eff9a0536a332ef191417" >> ${one_time_log}
+  echo -e "node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_02ce3171f470b3d638feeaec0b3f06bd27f86a26" >> ${one_time_log}
+  echo -e "node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_895ef49612e79d93c462c6d34abd8949b4c849af" >> ${one_time_log}
+  echo -e "node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_ecc26ef108c821f3aadc5e283c512ee68be7d43e" >> ${one_time_log}
+  echo -e "node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_488fea5101de4a8226718db0611c2ff2daeca06a" >> ${one_time_log}
+  echo -e "node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_7cd568771523a0621abff9ae3f95daf3a8694392" >> ${one_time_log}
+  node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_e315c111663af26a53e5fe4c82cc1baeecf50599
+  node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_c277de0546df31757ff26a723907bc150add4254
+  node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_75edfca17dfbe875e63a66633ed6b00e30adcb92
+  node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_9219f32e8de29b826faf44eb9b619788e29041bb
+  node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_0368eb926b115ecaf41eff9a0536a332ef191417
+  node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_02ce3171f470b3d638feeaec0b3f06bd27f86a26
+  node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_895ef49612e79d93c462c6d34abd8949b4c849af
+  node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_ecc26ef108c821f3aadc5e283c512ee68be7d43e
+  node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_488fea5101de4a8226718db0611c2ff2daeca06a
+  node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_7cd568771523a0621abff9ae3f95daf3a8694392
+
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_e315c111663af26a53e5fe4c82cc1baeecf50599" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_c277de0546df31757ff26a723907bc150add4254" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_75edfca17dfbe875e63a66633ed6b00e30adcb92" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_9219f32e8de29b826faf44eb9b619788e29041bb" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_0368eb926b115ecaf41eff9a0536a332ef191417" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_02ce3171f470b3d638feeaec0b3f06bd27f86a26" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_895ef49612e79d93c462c6d34abd8949b4c849af" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_ecc26ef108c821f3aadc5e283c512ee68be7d43e" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_488fea5101de4a8226718db0611c2ff2daeca06a" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_7cd568771523a0621abff9ae3f95daf3a8694392" >> ${one_time_log}
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_e315c111663af26a53e5fe4c82cc1baeecf50599
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_c277de0546df31757ff26a723907bc150add4254
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_75edfca17dfbe875e63a66633ed6b00e30adcb92
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_9219f32e8de29b826faf44eb9b619788e29041bb
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_0368eb926b115ecaf41eff9a0536a332ef191417
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_02ce3171f470b3d638feeaec0b3f06bd27f86a26
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_895ef49612e79d93c462c6d34abd8949b4c849af
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_ecc26ef108c821f3aadc5e283c512ee68be7d43e
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_488fea5101de4a8226718db0611c2ff2daeca06a
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_7cd568771523a0621abff9ae3f95daf3a8694392
 
 done
 
---
+++ api/ensureEventIndex.js
@@ -0,0 +1,26 @@
+var mongo = require('mongoskin');
+
+var db_host = process.argv[2] || null;
+var db_name = process.argv[3] || null;
+var collectionName = process.argv[4] || null;
+
+// exit if miss any argument
+if(!db_host || !db_name || !collectionName) {
+    console.log("Ensure Index: Invalid Parameters");
+    process.exit(1);
+}
+
+var dbBatchOptions = {safe: true, maxPoolSize: 1000};
+var dbLocalBatchName = db_host + "/" + db_name + '?auto_reconnect=true';
+var db = mongo.db(dbLocalBatchName, dbBatchOptions);
+var b_coll = db.collection(collectionName);
+
+b_coll.ensureIndex({"app_user_id":1}, function(err, nameIndex) {
+    if(err) {
+        console.log("Ensure Index with error: "+err);
+        db.close();
+        process.exit(1);
+    }
+    console.log("Finish ensure index of collection: " + collectionName + " with index name: " +nameIndex);
+    db.close();
+});
