1bee47ae943d5502d8d21cd81fc23934a93cd5d1
hadoop <hadoop@cat.pft.com>
2016-05-05 08:00:07 +0000

    [What] Fix createOEMShardKey_v3.sh bug

--- api/createOEMShardKey_v3.sh
+++ api/createOEMShardKey_v3.sh
@@ -87,26 +87,26 @@ for (( j = 0 ; j < ${#oems[@]} ; j++ )) do
   node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_488fea5101de4a8226718db0611c2ff2daeca06a
   node ensureSessionIndex.js ${dbHost} ${batchdb} raw_session_7cd568771523a0621abff9ae3f95daf3a8694392
 
-  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_e315c111663af26a53e5fe4c82cc1baeecf50599" >> ${one_time_log}
-  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_c277de0546df31757ff26a723907bc150add4254" >> ${one_time_log}
-  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_75edfca17dfbe875e63a66633ed6b00e30adcb92" >> ${one_time_log}
-  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_9219f32e8de29b826faf44eb9b619788e29041bb" >> ${one_time_log}
-  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_0368eb926b115ecaf41eff9a0536a332ef191417" >> ${one_time_log}
-  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_02ce3171f470b3d638feeaec0b3f06bd27f86a26" >> ${one_time_log}
-  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_895ef49612e79d93c462c6d34abd8949b4c849af" >> ${one_time_log}
-  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_ecc26ef108c821f3aadc5e283c512ee68be7d43e" >> ${one_time_log}
-  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_488fea5101de4a8226718db0611c2ff2daeca06a" >> ${one_time_log}
-  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_7cd568771523a0621abff9ae3f95daf3a8694392" >> ${one_time_log}
-  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_e315c111663af26a53e5fe4c82cc1baeecf50599
-  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_c277de0546df31757ff26a723907bc150add4254
-  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_75edfca17dfbe875e63a66633ed6b00e30adcb92
-  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_9219f32e8de29b826faf44eb9b619788e29041bb
-  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_0368eb926b115ecaf41eff9a0536a332ef191417
-  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_02ce3171f470b3d638feeaec0b3f06bd27f86a26
-  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_895ef49612e79d93c462c6d34abd8949b4c849af
-  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_ecc26ef108c821f3aadc5e283c512ee68be7d43e
-  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_488fea5101de4a8226718db0611c2ff2daeca06a
-  node ensureEventIndex.js ${dbHost} ${batchdb} raw_session_7cd568771523a0621abff9ae3f95daf3a8694392
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_e315c111663af26a53e5fe4c82cc1baeecf50599" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_c277de0546df31757ff26a723907bc150add4254" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_75edfca17dfbe875e63a66633ed6b00e30adcb92" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_9219f32e8de29b826faf44eb9b619788e29041bb" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_0368eb926b115ecaf41eff9a0536a332ef191417" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_02ce3171f470b3d638feeaec0b3f06bd27f86a26" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_895ef49612e79d93c462c6d34abd8949b4c849af" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_ecc26ef108c821f3aadc5e283c512ee68be7d43e" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_488fea5101de4a8226718db0611c2ff2daeca06a" >> ${one_time_log}
+  echo -e "node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_7cd568771523a0621abff9ae3f95daf3a8694392" >> ${one_time_log}
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_e315c111663af26a53e5fe4c82cc1baeecf50599
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_c277de0546df31757ff26a723907bc150add4254
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_75edfca17dfbe875e63a66633ed6b00e30adcb92
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_9219f32e8de29b826faf44eb9b619788e29041bb
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_0368eb926b115ecaf41eff9a0536a332ef191417
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_02ce3171f470b3d638feeaec0b3f06bd27f86a26
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_895ef49612e79d93c462c6d34abd8949b4c849af
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_ecc26ef108c821f3aadc5e283c512ee68be7d43e
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_488fea5101de4a8226718db0611c2ff2daeca06a
+  node ensureEventIndex.js ${dbHost} ${batchdb} raw_event_7cd568771523a0621abff9ae3f95daf3a8694392
 
 done
 
