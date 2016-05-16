16cd1b53e877bd6b371e74742fa87c879eb34d9c
hadoop <hadoop@cat.pft.com>
2016-05-16 09:34:02 +0000

    [What] kafka server reduce to 1 host

--- api/api.js
+++ api/api.js
@@ -44,6 +44,7 @@ var reconnectInterval = null;
 var zkList = '172.31.19.126:2181,172.31.27.99:2181,172.31.27.76:2181';  // bootstrap.servers
 
 zkList = '172.31.19.74:2181,172.31.27.99:2181,172.31.22.124:2181';  // bootstrap.servers
+zkList = '172.31.19.74:2181';  // bootstrap.servers
 var client = new Client(zkList);
 
 //var p = argv.p || 0; // default is 0
