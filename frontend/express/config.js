3787db0ffed0abe874029ff5019454b6622f026a
web-test1 <web-test1@aws.wet-test1>
2016-08-02 11:50:48 +0000

    [What] add --max_old_space_size=1024 when start node        add config files for web-test branch

---
+++ frontend/express/config.js
@@ -0,0 +1,28 @@
+var countlyConfig = {
+    mongodb: {
+//        host: "172.31.15.158",
+        host: "172.31.26.154",
+        //host: "localhost",
+        db: "countly_OEM",
+        port: 27017
+    },
+    /*  or for a replica set
+    mongodb: {
+        replSetServers : [
+            '192.168.3.1:27017/?auto_reconnect=true',
+            '192.168.3.2:27017/?auto_reconnect=true'
+        ],
+        db: "countly",
+    },
+    */
+    /*  or define as a url
+    mongodb: "localhost:27017/countly?auto_reconnect=true",
+    */
+    web: {
+        port: 6001,
+        host: "localhost",
+        use_intercom: true
+    }
+};
+
+module.exports = countlyConfig;
