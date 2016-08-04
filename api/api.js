176d1e59b2772d926737a8b153cf773357ce614c
hadoop <hadoop@cat.pft.com>
2016-08-04 08:42:43 +0000

    [What] enable send to kafka match in user table

--- api/api.js
+++ api/api.js
@@ -245,7 +245,7 @@ function sendKafka(data, key, isSession) {
         //console.log(checkABTest);
         if (checkABTest) {
             //console.log("This Device ID in ABTesting");
-            if (0) {
+            if (1) {
             producer.send([
                 { topic: ABTestTopicName, partition: (randomCnt%partitionNum), messages: JSON.stringify(data)}
             ], kafkaCB);
