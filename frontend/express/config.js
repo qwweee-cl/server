ffc764ffe2e18a1af0a9b0d6edb09ccda0b9b2e2
hadoop <hadoop@cat.pft.com>
2016-09-21 08:19:34 +0000

    [What] change mongodb ip show dashboard

--- frontend/express/config.js
+++ frontend/express/config.js
@@ -1,7 +1,7 @@
 var countlyConfig = {
     mongodb: {
 //        host: "172.31.15.158",
-        host: "172.31.12.93",
+        host: "172.31.24.236",
         //host: "localhost",
         db: "countly_OEM",
         port: 27017
