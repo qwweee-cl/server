209bb2108a0108bdb799f53064aaef9017721dfb
hadoop <hadoop@cat.pft.com>
2016-09-22 08:01:46 +0000

    [What] change update per day

--- api/api.js
+++ api/api.js
@@ -123,7 +123,7 @@ var schedule = require('node-schedule');
 var isUpdating = false;
 
 //var job = schedule.scheduleJob('30 07 */1 * *', function(){
-var job = schedule.scheduleJob('*/30 * * * *', function(){
+var job = schedule.scheduleJob('0 0 * * *', function(){
     console.log('Call update ABTesting Table!');
     updateABTesting();
 });
