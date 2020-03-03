1901e4432721e841c483d6acd7c529419c7eb7c6
hadoop <hadoop@cat.pft.com>
2020-03-03 02:18:37 +0000

    [What] add YCT iOS's appkey and topic.

--- api/api.js
+++ api/api.js
@@ -145,7 +145,7 @@ var topicList = ['Node_Event_BCS_And', 'Node_Event_BCS_iOS', 'Node_Event_OtherAp
   'Node_Session_YMK4B_And', 'Node_Session_YMK4B_iOS', 'Node_Event_YMK4B_And', 'Node_Event_YMK4B_iOS',
   'Node_Session_YMK4BTrial_And', 'Node_Session_YMK4BTrial_iOS', 'Node_Event_YMK4BTrial_And', 'Node_Event_YMK4BTrial_iOS',
   'Node_Session_YMK4BBrand_And', 'Node_Session_YMK4BBrand_iOS', 'Node_Event_YMK4BBrand_And', 'Node_Event_YMK4BBrand_iOS',
-  'Node_Session_AmwayCN_AEC_iOS', 'Node_Event_AmwayCN_AEC_iOS'
+  'Node_Session_AmwayCN_AEC_iOS', 'Node_Event_AmwayCN_AEC_iOS', 'Node_Session_YCT_iOS', 'Node_Event_YCT_iOS'
 ];
 
 function producerReady() {
@@ -271,6 +271,7 @@ var appMap = {
   "bf6bad03e0833e6e432a74136a01809bb3b980d4": {appName: "YMK4BBrand", appOS: "iOS"}, // YMK4BBrand_iOS
   "fd129a84a9ae650fb18837e6cba7b69c6ec358da": {appName: "AmwayCN_AEC", appOS: "iOS"}, // AmwayCN_AEC_iOS
   "6993b74968d8ecdaa3b3c633beca800de732381a": {appName: "YCV", appOS: "And"}, // YouCamVideo_Android
+  "54e26029b464a4a2d15ccfd237049c94db09ca36": {appName: "YCT", appOS: "iOS"}, // YouCamToolkit_iOS
   "251ff0b79d64016f72896891275fed15caa231f8": {appName: "Aphrodite", appOS: "And", sdk: true}, // Aphrodite_Android
   "c210e235a2dd64d7625b793443de2d7ab2424ae3": {appName: "Aphrodite", appOS: "iOS", sdk: true}, // Aphrodite_iOS
   "3c8be6a417c8e010ca308f1764bcb1542ce737b2": {appName: "MaryKay_China", appOS: "And", sdk: true}, // MaryKay_China_Android
--- api/appKey.js
+++ api/appKey.js
@@ -24,6 +24,7 @@ var appKey = {};
 		"YouCam_Store_And"			: "fa9cbde99587e00afcbd7a9c834e78b5185b8065",
 		"YouCam_Store_iOS"			: "9357f63f387a02872cae14f8539b37cc37404727",
 		"YouCam_Video_And"		: "6993b74968d8ecdaa3b3c633beca800de732381a",
+		"YouCam_Toolkit_And"		: "54e26029b464a4a2d15ccfd237049c94db09ca36",
 		"Amway_And"			: "9f093278d98ad4159a6df2f29d021e6e34649747",
 		"Amway_iOS"			: "1a89a46c0465a15ce57f3709ca01c2c9fb36feb4",
 		"Belcorp_Skincare_And"		: "ab8ada3e1f156663453cee32debe3292fba834ae",
