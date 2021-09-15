e0180bcd4b9f0ba8526bc8178c43a82eb96886b2
hadoop <hadoop@cat.pft.com>
2021-09-15 10:13:53 +0000

    Revert "[What] configuration a kafka-demo for B2B data be sent."
    This reverts commit d762dfdac49bca3eb9495accb661bc02a1d4f5e6.

--- api/api.js
+++ api/api.js
@@ -41,8 +41,7 @@ var Client = kafka.Client;
 
 //var zkList = '172.31.27.186:2181,172.31.27.187:2181,172.31.27.188:2181';  // bootstrap.servers
 //var zkList = '172.31.16.236:2181,172.31.16.237:2181,172.31.16.238:2181,172.31.16.239:2181';  // bootstrap.servers
-//var zkList = '172.31.25.82:2181,172.31.30.167:2181,172.31.29.255:2181,172.31.26.160:2181';  // bootstrap.servers
-var zkList = '172.31.7.135:3181';  // bootstrap.servers
+var zkList = '172.31.25.82:2181,172.31.30.167:2181,172.31.29.255:2181,172.31.26.160:2181';  // bootstrap.servers
 var timeToRetryConnection = 12 * 1000; // 12 seconds
 var reconnectInterval = null;
 var kafkaErrorCount = 0;
@@ -71,8 +70,7 @@ var cando = false;
 var nokafkaErrorCount = 0;
 var nokafkaerrorContext = "";
 var isNoKafka = true;
-//var kafkaList = '172.31.25.82:9092,172.31.30.167:9092,172.31.29.255:9092,172.31.26.160:9092';  // bootstrap.servers
-var kafkaList = '172.31.7.135:9092';  // bootstrap.servers
+var kafkaList = '172.31.25.82:9092,172.31.30.167:9092,172.31.29.255:9092,172.31.26.160:9092';  // bootstrap.servers
 
 var noKafka = require('no-kafka');
 var noKafkaProducer = new noKafka.Producer({
@@ -155,7 +153,6 @@ var topicList = ['Node_Event_BCS_And', 'Node_Event_BCS_iOS', 'Node_Event_OtherAp
   'Node_Event_WCM-Wechat-Mini_Web',
   'Node_Event_YCP_CN_And', 'Node_Event_YCP_CN_iOS', 'Node_Event_YMK_CN_And', 'Node_Event_YMK_CN_iOS',
   'Node_Session_YCP_CN_And', 'Node_Session_YCP_CN_iOS', 'Node_Session_YMK_CN_And', 'Node_Session_YMK_CN_iOS',
-  'B2B_Event', 'B2B_Session',
 ];
 
 function producerReady() {
