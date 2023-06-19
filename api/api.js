ad2f7310fbadaae0766be1cb6d3487acbcf5ca86
Gary_Huang <gary_huang@perfectcorp.com>
2023-06-19 11:50:49 +0800

    [what] check if config not include geo json object.

--- api/api.js
+++ api/api.js
@@ -905,7 +905,7 @@ function insertRawColl(coll, eventp, params, isSession) {
     return;
   }
 
-  common.computeGeoInfo(eventp, common.config.geo.showIP);
+  common.computeGeoInfo(eventp, (common.config.geo && common.config.geo.showIP));
   var appkey = eventp.app_key;
   var checkAppKey = jsonQuery(['[key=?]', appkey], {data: appKeyMaps}).value;
   if (!checkAppKey) {
